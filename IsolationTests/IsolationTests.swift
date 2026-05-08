19dedb4973a1883213e86df84e102b45e273406b
Kasra Babaei <kbabaei@opentable.com>
2026-05-08 09:23:16 -0400

    Initial Commit

---
+++ IsolationTests/IsolationTests.swift
@@ -0,0 +1,19 @@
+//
+//  IsolationTests.swift
+//  IsolationTests
+//
+//  Created by Kasra Babaei on 2026-05-08.
+//
+
+import Testing
+@testable import Isolation
+
+struct IsolationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
