68318a867b993480a6e979be035f879824d35069
Kasra Babaei <kasrababaei@gmail.com>
2026-05-31 15:38:55 -0400

    Added failing test to BankTests.swift:13

--- IsolationTests/BankTests.swift
+++ IsolationTests/BankTests.swift
@@ -9,27 +9,29 @@ import Testing
 import Foundation
 @testable import Isolation
 
-@Suite(.serialized)
-@MainActor
-final class BankTests {
-    private var bank: Bank!
+@Suite
+struct BankTests {
+  @Test func basics() throws {
+    let bank = Bank()
+    let id1 = bank.openAccount(initialDeposit: 100)
+    let id2 = bank.openAccount(initialDeposit: 100)
+    try bank.transfer(amount: 50, from: id1, to: id2)
+    #expect(bank.totalDeposits == 200)
+    #expect(try bank.account(for: id1).balance == 50)
+    #expect(try bank.account(for: id2).balance == 150)
+  }
 
-    init() {
-        setup()
-    }
-
-    private func setup() {
-        DispatchQueue.main.async {
-            self.bank = Bank()
+  // BankAccount.swift:29
+  // | - Task 29: EXC_BAD_ACCESS (code=1, address=0x10)
+  @Test func newAccountRush() async {
+    let bank = Bank()
+    await withTaskGroup { group in
+      for _ in 1...1000 {
+        group.addTask {
+          bank.openAccount(initialDeposit: 100)
         }
+      }
     }
-
-    @Test func basics() throws {
-        let id1 = bank.openAccount(initialDeposit: 100)
-        let id2 = bank.openAccount(initialDeposit: 100)
-        try bank.transfer(amount: 50, from: id1, to: id2)
-        #expect(bank.totalDeposits == 200)
-        #expect(try bank.account(for: id1).balance == 50)
-        #expect(try bank.account(for: id2).balance == 150)
-    }
+    #expect(bank.totalDeposits == 100 * 1000)
+  }
 }
